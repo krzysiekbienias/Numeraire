543cb0418eed33a2e096af31ac1601de245a1ee6
Krzysiek Bienias <krzysiek.bienias.gmail.com>
2023-11-11 11:16:28 +0100

    Adding tool kit: QuantliB wrapper, excel handler using XlWings, and DB handler.

---
+++ geralt/src/geralt/tool_kit/data_base_handler.py
@@ -0,0 +1,19 @@
+
+class DataBaseInterface:
+
+    def __init__(self) -> None:
+        pass
+    
+    def make_connection(self):
+        pass
+
+    def fetch_data(self):
+        pass
+
+    def insert_data(self):
+        pass
+        
+        
+
+
+
