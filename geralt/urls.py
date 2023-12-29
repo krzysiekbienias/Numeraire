e40f27a2281b8c7642a99a7c19012c2df83cff51
Krzysiek Bienias <krzysiek.bienias.gmail.com>
2023-12-29 14:45:08 +0100

    extend urlpatterns list to provide single trade url

--- geralt/urls.py
+++ geralt/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[path("", views.starting_page, name="starting-page"),
-             path("trade_book",views.trade_details, name="trade-book"),
-             path("pricing environment", views.pricing_environment), 
-             path("pricing environment/<slug>", views.pricing_environment)]
+             path("trade_book",views.prepare_form, name="trade-book"),
+             path("trade_book/<int:trade_id>",views.single_trade, name="single-trade"),
+             path("pricing environment", views.pricing_environment,name="pricing-environment")] 
