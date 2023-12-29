49b833f134203886d297be09c0ba62bcb4b62891
Krzysiek Bienias <krzysiek.bienias.gmail.com>
2023-12-29 14:38:17 +0100

    add prepare_form view to display all trades from data base. Add single_tred function to provide more specyfic information about trade.

--- geralt/views.py
+++ geralt/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseNotFound
+from django.http import HttpResponse,HttpResponseNotFound,HttpResponseRedirect,JsonResponse
+from django.core.serializers import serialize
 from django.template.loader import render_to_string
-from .models import Person
+import json
+from .models import Person,TradeBookModel
+from .src.geralt.pricing_environment.pricing_environment import TradeCalendarSchedule,MarketEnvironment
+from .forms import CompanyForm
+
 
 
 # Create your views here.
@@ -39,15 +44,45 @@ trades_all=  [{
     "Dividend": 0
   }]
 
+# --------------------------
+# Region source code objectsvis
+# --------------------------
 
+
+
+# --------------------------
+# End Region source code objects
+# --------------------------
+
+
+# --------------------------
+# Region Rendering page app
+# --------------------------
 def starting_page(request):
+    
     return render(request,"geralt/index.html")
 
 
-def trade_details(request):
-    return render(request,"geralt/trade_book.html",{"trades":trades_all}) 
+def prepare_form(request):
+    
+
+    all_trades=TradeBookModel.objects.all()
+    serialized_data=serialize("json",all_trades)
+    serialized_data=json.loads(serialized_data)
+    return render(request,"geralt/trade_book.html",{"trades":serialized_data}) 
+
+def single_trade(request,trade_id:int):
+    single_trade=TradeBookModel.objects.get(pk=trade_id)
+    return render(request,'geralt/single_trade.html',{"single_trade":single_trade})
+
+  
+    
 
     
                  
 def pricing_environment(request):
     pass
+
+# --------------------------
+# End Region Rendering page app
+# --------------------------
