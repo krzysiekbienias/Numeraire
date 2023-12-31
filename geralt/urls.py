741da5ace86d6c2abe89df9b63f8ffb04fae19cf
Krzysiek Bienias <krzysiek.bienias.gmail.com>
2023-12-31 14:49:30 +0100

    Fix crispy forms importing and adding namespace to urls include.

--- geralt/urls.py
+++ geralt/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name='geralt'
+
 urlpatterns=[path("", views.starting_page, name="starting-page"),
              path("trade_book",views.prepare_form, name="trade-book"),
              path("trade_book/<int:trade_id>",views.single_trade, name="single-trade"),
